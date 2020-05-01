eb9d9199a056d4bd7b6ee6585aa536dd39d283ba
ali <ali.marinas@icloud.com>
2020-05-01 16:37:43 -0700

    test case

--- cypress/integration/Test1.js
+++ cypress/integration/Test1.js
@@ -1,5 +1,8 @@
+///<reference types="Cypress"/>
+
 describe("First Test", function () {
   it("Does not do much", function () {
     cy.visit("http://google.com");
+    cy.get(".gLFyf").type("cat pictures");
   });
 });
